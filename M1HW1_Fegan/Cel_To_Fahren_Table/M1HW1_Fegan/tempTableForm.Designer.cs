f7c390bb87c4e1b7b007d88f266a461245613ffe
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2021-08-18 09:31:05 -0400

    Add files via upload

---
+++ M1HW1_Fegan/Cel_To_Fahren_Table/M1HW1_Fegan/tempTableForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace M1HW1_Fegan
+{
+    partial class tempTableForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.tempConvertListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(12, 234);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(87, 33);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(213, 236);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(65, 33);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // tempConvertListBox
+            // 
+            this.tempConvertListBox.FormattingEnabled = true;
+            this.tempConvertListBox.ItemHeight = 16;
+            this.tempConvertListBox.Location = new System.Drawing.Point(12, 12);
+            this.tempConvertListBox.Name = "tempConvertListBox";
+            this.tempConvertListBox.Size = new System.Drawing.Size(266, 212);
+            this.tempConvertListBox.TabIndex = 2;
+            // 
+            // tempTableForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 281);
+            this.Controls.Add(this.tempConvertListBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "tempTableForm";
+            this.Text = "Temp Table";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ListBox tempConvertListBox;
+    }
+}
+
---
+++ M1HW1_Fegan/Cel_To_Fahren_Table/M1HW1_Fegan/tempTableForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* 08-17-2021
+* CSC 253
+* Zachary Fegan
+* This program will display a table that converts different Celsius temperatures into their Fahrenheit equivalents temperatures.
+*/
+
+namespace M1HW1_Fegan
+{
+    public partial class tempTableForm : Form
+    {
+        public tempTableForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // These variables will hold their values when iterating through the while loop.
+            int cel = 0;
+            decimal fahren;
+
+            // This while loop will iterate Celsius temperatures 0 through 20.
+            while (cel <= 20)
+            {
+                // This is the formula that will calculate the celsius temperatures to their fahrenheit equivalents temperatures.
+                fahren = ((9 / 5) * cel) + 32;
+
+                // This will display the items to the user via list box.
+                tempConvertListBox.Items.Add("Celsius: " + cel + "                " +
+                                             "Fahrenheit: " + fahren);
+
+                // Used to incriment Celsius variables.
+                cel++;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // This is to close the form.
+            this.Close();
+        }
+    }
+}
