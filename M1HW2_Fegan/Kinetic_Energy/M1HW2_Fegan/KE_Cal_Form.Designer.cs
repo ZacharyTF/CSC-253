0de77b3e0972529166b747ee6c3f72ee8ec8f7f8
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2021-09-24 17:23:23 -0400

    Add files via upload

---
+++ M1HW2_Fegan/Kinetic_Energy/M1HW2_Fegan/KE_Cal_Form.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace M1HW2_Fegan
+{
+    partial class KE_Cal_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.kinEnergyLabelBox = new System.Windows.Forms.Label();
+            this.massTextBox = new System.Windows.Forms.TextBox();
+            this.velocityTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Object\'s Mass:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Object\'s Velocity: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "(in kilograms)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "(in meters per second)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Object\'s kinetic energy";
+            // 
+            // displayButton
+            // 
+            this.displayButton.Location = new System.Drawing.Point(87, 172);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(76, 36);
+            this.displayButton.TabIndex = 5;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(169, 172);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(76, 36);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(251, 172);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(76, 36);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // kinEnergyLabelBox
+            // 
+            this.kinEnergyLabelBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.kinEnergyLabelBox.Location = new System.Drawing.Point(181, 130);
+            this.kinEnergyLabelBox.Name = "kinEnergyLabelBox";
+            this.kinEnergyLabelBox.Size = new System.Drawing.Size(186, 22);
+            this.kinEnergyLabelBox.TabIndex = 8;
+            // 
+            // massTextBox
+            // 
+            this.massTextBox.Location = new System.Drawing.Point(181, 26);
+            this.massTextBox.Name = "massTextBox";
+            this.massTextBox.Size = new System.Drawing.Size(186, 22);
+            this.massTextBox.TabIndex = 9;
+            // 
+            // velocityTextBox
+            // 
+            this.velocityTextBox.Location = new System.Drawing.Point(181, 80);
+            this.velocityTextBox.Name = "velocityTextBox";
+            this.velocityTextBox.Size = new System.Drawing.Size(186, 22);
+            this.velocityTextBox.TabIndex = 10;
+            // 
+            // KE_Cal_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 238);
+            this.Controls.Add(this.velocityTextBox);
+            this.Controls.Add(this.massTextBox);
+            this.Controls.Add(this.kinEnergyLabelBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "KE_Cal_Form";
+            this.Text = "KE Calculator Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label kinEnergyLabelBox;
+        private System.Windows.Forms.TextBox massTextBox;
+        private System.Windows.Forms.TextBox velocityTextBox;
+    }
+}
+
---
+++ M1HW2_Fegan/Kinetic_Energy/M1HW2_Fegan/KE_Cal_Form.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+* 08-18-2021
+* CSC 253
+* Zachary Fegan
+* This program will calculate an object's kinetic energy, and it will display that information to the user.
+*/
+
+namespace M1HW2_Fegan
+{
+    public partial class KE_Cal_Form : Form
+    {
+        public KE_Cal_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            // These variables will hold each the object's mass, velocity, and its kinetic energy.
+            double mass, velocity, kinEnergy;
+
+            // This will translate what text the user had entered into the mass textbox into numerical data.
+            if(double.TryParse(massTextBox.Text, out mass))
+            {
+                // This will translate what text the user had entered into the velocity textbox into numerical data.
+                if (double.TryParse(velocityTextBox.Text, out velocity))
+                {
+                    // Will calculate the fomula in a value-returning method.
+                    kinEnergy = getKineticEnergy(mass, velocity);
+
+                    // This will put the results into a label box that the user cannot edit.
+                    kinEnergyLabelBox.Text = kinEnergy.ToString();
+                }
+                else
+                {
+                    // Display to the user to enter a number that is valid.
+                    MessageBox.Show("Enter a vaild number.");
+                }
+            }
+            else
+            {
+                // Display to the user to enter a number that is valid.
+                MessageBox.Show("Enter a vaild number.");
+            }
+        }
+
+        // This is where the formula will be calculated.
+        private double getKineticEnergy(double mass, double velocity)
+        {
+            // This formula is used to calculate the object's Kinetic Energy.
+            // This will also return the 
+            return 0.5 * mass * (velocity * velocity);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // This is to clear any text/input that is contained in these boxes
+            massTextBox.Text = "";
+            velocityTextBox.Text = "";
+            kinEnergyLabelBox.Text = "";
+
+            // When the user clicks the clear button it will take the user back to
+            // the first text box.
+            massTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // This is used to close this form
+            this.Close();
+        }
+    }
+}
